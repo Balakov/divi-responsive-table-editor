bb056c7d408c6f36d2f4a106976b2600552102b6
Balakov <mike@mikestimpson.com>
2025-07-20 11:44:41 +0100

    Bump version

--- responsive-table-editor.php
+++ responsive-table-editor.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Responsive Table Editor for Divi
  * Description: A Divi module to preview responsive HTML tables from CSV input.
- * Version: 1.10
+ * Version: 1.11
  * Author: Mike Stimpson
  */
 
